31751a0da7fbc34430d62b0a228a3745dc137166
Ali Sasani <alisasanibab@gmail.com>
2022-07-07 15:49:47 +0430

    add api to recieve single recipe based on its id value

---
+++ pages/api/recipes/[id].ts
@@ -0,0 +1,19 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { recipes } from '../../../data/recipes';
+import { RecipeProp } from '../../../common/types';
+type Data = {
+  // name: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { id } = req.query;
+  const selectedRecipe: RecipeProp = recipes.filter(
+    ({ id }: { id: number }) => id == Number(id)
+  )[0];
+
+  res.status(200).json(selectedRecipe);
+}
